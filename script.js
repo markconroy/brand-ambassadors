6a1d6d7cfdc0e8e554d87eb3f88d6531c54f7ecc
Mark Conroy <mark@mark.ie>
2024-03-01 06:17:27 +0000

    adds basic code to convert container to base64 image

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const whoSaidItInput = document.querySelector('.input-who');
 const whatWasSaid = document.querySelector('.what-was-said');
 const whoSaidIt = document.querySelector('.who-said-it');
 const postButton = document.querySelector('.post-button');
+const imageContainer = document.querySelector('#image');
 
 whatWasSaidInput.addEventListener('input', () => {
   console.log(whatWasSaidInput.value);
@@ -13,3 +14,10 @@ whatWasSaidInput.addEventListener('input', () => {
 whoSaidItInput.addEventListener('input', () => {
   whoSaidIt.textContent = whoSaidItInput.value;
 });
+
+postButton.addEventListener('click', () => {
+  html2canvas(document.querySelector("#container")).then(canvas => {
+    const base64image = canvas.toDataURL("image/png");
+    image.querySelector('img').src = base64image;
+  });
+})
