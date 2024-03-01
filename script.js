2edec5e2129597e44f61afa9a92169ccc406f8e3
Mark Conroy <mark@mark.ie>
2024-03-01 06:03:34 +0000

    adds text input

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+const message = document.querySelector('.input-message');
 const whatWasSaidInput = document.querySelector('.input-what');
 const whoSaidItInput = document.querySelector('.input-who');
 const whatWasSaid = document.querySelector('.what-was-said');
 const whoSaidIt = document.querySelector('.who-said-it');
+const postButton = document.querySelector('.post-button');
 
 whatWasSaidInput.addEventListener('input', () => {
   console.log(whatWasSaidInput.value);
@@ -11,4 +13,3 @@ whatWasSaidInput.addEventListener('input', () => {
 whoSaidItInput.addEventListener('input', () => {
   whoSaidIt.textContent = whoSaidItInput.value;
 });
-
